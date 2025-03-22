96886858866650d75dad5b7707a55e511c46c06e
nhidh235802 <duongnhi2932@gmail.com>
2025-03-23 00:15:22 +0700

    src DigitalVideoDisc

---
+++ Lab02/src/Cart.java
@@ -0,0 +1,10 @@
+
+public class Cart {
+	
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOredered[] =
+			new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0;
+	
+	
+}
